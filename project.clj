5e56bd4efb2d5b80131ef73834ac2ed3dfdf6700
Rodrigo Landerdahl <rlanderdahl@gmail.com>
2012-12-14 19:12:49 -0200

    Add description and version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jregex "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject clj-jregex "0.0.1"
+  :description "JRegex wrapper."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [net.sourceforge.jregex/jregex "1.2_01"]])
